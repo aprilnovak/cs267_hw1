115f99f62aaca7df7f7bbd4e238e0fe19e3ca4fe
April Novak <novak@berkeley.edu>
2017-01-30 09:45:01 -0800

    implemented copy optimization in dgemm-naive.c

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -1,3 +1,6 @@
+#include <stdlib.h> // For: malloc
+#include <stdio.h> // For: printf
+
 const char* dgemm_desc = "Naive, three-loop dgemm.";
 
 /* This routine performs a dgemm operation
@@ -6,6 +9,19 @@ const char* dgemm_desc = "Naive, three-loop dgemm.";
  * On exit, A and B maintain their input values. */    
 void square_dgemm (int n, double* A, double* B, double* C)
 {
+  // transpose A so that it is row-major
+  double * aij = 0;
+  aij = (double*) malloc(n * n * sizeof(double));
+  
+  if (aij == NULL)
+    printf("Memory allocation error!");
+
+  for (int i = 0; i < n; ++i)
+  {
+    for (int j = 0; j < n; ++j)
+        aij[i + j*n] = A[j + i*n];    
+  }
+    
   /* For each row i of A */
   for (int i = 0; i < n; ++i)
     /* For each column j of B */
@@ -15,7 +31,9 @@ void square_dgemm (int n, double* A, double* B, double* C)
       double cij = C[i+j*n];
       for( int k = 0; k < n; k++ )
         //C[i+j*n] += A[i+k*n] * B[k+j*n];
-        cij += A[i+k*n] * B[k+j*n];
+        //cij += A[i+k*n] * B[k+j*n];
+        cij += aij[k+i*n] * B[k+j*n];
       C[i+j*n] = cij;
     }
+  free(aij);
 }
