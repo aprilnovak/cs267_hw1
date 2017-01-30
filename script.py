33ab7269a510125d918a0767837873ec67ad1c5a
April Novak <novak@berkeley.edu>
2017-01-29 16:37:17 -0800

    extracted data.

--- script.py
+++ script.py
@@ -1,6 +1,26 @@
 import sys
 
 filename = sys.argv[1]
+description = sys.argv[2]
 
 with open(filename, 'r') as f:
-    print(filename)
+    size = []
+    Mflops = []
+    percentage = []
+    my_dict = {'description': description}
+
+    for line in f:
+        data = line.split('\t')
+        print(data)
+        try:
+            size.append(data[0].split(':')[1])
+            Mflops.append(data[1].split(':')[1])
+            percentage.append(data[2].split(':')[1])
+        # the last line of the file will not have two entires, so except it
+        except IndexError:
+            pass
+ 
+    my_dict['size'] = size
+    my_dict['Mflops'] = Mflops
+    my_dict['percentage'] = percentage
+    print(my_dict['Mflops'])
